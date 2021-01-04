9502ac9000d1055fa558a7fc14256e9772c2ac72
chezynick <chezynickannie@yahoo.co.uk>
2021-01-04 09:56:25 +0000

    deleted icons and images from boilerplate

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Wunderlust 2006-2020`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `A pretty basic travel blog page to test out Gatsby.`,
+    author: `nick chesterton`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -28,7 +28,6 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
